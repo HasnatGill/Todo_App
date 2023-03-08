2f66cf4b047557be6d79d3d033d1c4e5666e37d1
Hasnat Majid <107733484+HasnatGill@users.noreply.github.com>
2023-03-08 13:50:17 +0500

    tweaks

--- src/component/PrivateRoute.js
+++ src/component/PrivateRoute.js
@@ -1,8 +1,6 @@
 import React, { useContext } from 'react'
 import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext'
-import Login from '../pages/Auth/LoginAndResgister'
-// import { Link } from 'react-router-dom'
 
 export default function PrivateRoute(props) {
 
